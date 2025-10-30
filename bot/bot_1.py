bcb8d446c9436bed9978f308903d41b61e914b15
Zanillect58 <andres.galeano@usantoto.edu.co>
2025-10-29 21:17:45 -0500

    mi primer bot

---
+++ bot/bot_1.py
@@ -0,0 +1,13 @@
+import  MetaTrader5 as mt5
+import pandas as pd
+
+path = r'C:\Program Files\RoboForex MT5 Terminal\terminal64.exe'
+nombre = 67106046
+contraseña = 'Sebas.123'
+server = 'RoboForex-ECN'    
+mt5.initialize(login=nombre, password=contraseña, server=server, path=path)
+
+rates = mt5.copy_rates_from_pos('XAUUSD', mt5.TIMEFRAME_M1, 0,9999)
+tabla_precios = pd.DataFrame(rates)
+tabla_precios['time'] = pd.to_datetime(tabla_precios['time'], unit='s')
+print(tabla_precios.head())
